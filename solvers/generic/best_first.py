93c67523b8f65c8ee1fd281c70f4da049fb82af6
Apoorva Singh <asingh@student.agh.edu.pl>
2022-12-19 00:57:04 +0000

    typo best first pop

--- solvers/generic/best_first.py
+++ solvers/generic/best_first.py
@@ -60,7 +60,7 @@ class BestFirstSearch:
         self.frontier.push(self.root)
 
         while not self.frontier.empty():
-            curr_node = self.frontier.get()
+            curr_node = self.frontier.pop()
             if self.problem.is_goal(curr_node.state):
                 return curr_node
             
