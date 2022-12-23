10c62d8969e9422aac5c833c7de1f34ee40aa5ad
Apoorva Singh <asingh@student.agh.edu.pl>
2022-12-23 11:14:40 +0000

    blocks world problem and heuristic try

--- problems/blocks_world/blocks_world_heuristic.py
+++ problems/blocks_world/blocks_world_heuristic.py
@@ -16,60 +16,46 @@ class BlocksWorldNaiveHeuristic(Heuristic):
         # TODO:
         # return a dict of form:
         # { <block name> : <index of column in the goal state> }
-        columns = {
-            "1" : "0",
-            "2" : "1",
-            "3" : "2",
-            "4" : "3",
-            "5" : "4",
-            "6" : "5",
-            "7" : "6",
-            "8" : "7"
-        }
-        return columns
-        raise NotImplementedError
+        result = {}
+        for column in range(0, len(goal.columns)):
+            if goal.columns[column]:
+                for block in goal.columns[column]:
+                    result.update({block: column})
+        return result
+
 
     def _calculate_expected_fundaments(self, goal: BlocksWorldState) -> Dict[str, List[str]]:
         # TODO:
         # return a dict of form:
         # { <block name> : <list of the blocks below it in the goal state> }
+        result = {}
+        for column in goal.columns:
+            for block in range(0, len(column)):
+                below = column[:block]
+                result.update({column[block]: below})
+        return result
 
-
-#------------------{
-        Dict[str, List[str]]:
-            fundaments = {
-                "1": [],
-                "2": ["1"],
-                "3": ["1", "2"],
-                "4": [],
-                "5": [],
-                "6": [],
-                "7": ["6"],
-                "8": []
-            }
-        return fundaments
-
-        raise NotImplementedError
 
     def __call__(self, state: BlocksWorldState) -> int:
         # TODO:
         # - add `1` to the heuristic value per each block placed in an incorrect column
         # - for other blocks, add `2` if their fundament is incorrect 
         # tip. use self.expected_columns and self.expected_fundaments
+        current_columns = self._calculate_expected_columns(state)
+        current_fundaments = self._calculate_expected_fundaments(state)
+        columns_difference = 0
+        fundaments_difference = 0
+        not_needed_blocks = []
+        for block in current_columns:
+            if current_columns[block] != self.expected_columns[block]:
+                columns_difference += 1
+                not_needed_blocks.append(block)
 
+        for block in current_fundaments:
+            if block in not_needed_blocks:
+                continue
+            if current_fundaments[block] != self.expected_fundaments[block]:
+                fundaments_difference += 1
 
-#------------------{
+        return columns_difference + fundaments_difference * 2
 
-        heuristic = 0
-        for block in state.columns[0]:
-            if self.expected_columns[block] != 0:
-                heuristic += 1
-        for column in state.columns[1:]:
-            for block in column:
-                fundament = state.fundament[block]
-                if fundament is not None and fundament not in self.expected_fundaments[block]:
-                    heuristic += 2
-        return heuristic
-
-        raise NotImplementedError
-#------------------------------}
